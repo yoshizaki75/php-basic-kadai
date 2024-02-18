260b79e36cd225f4e6bcfe75f53a8f4b5de2b1be
yoshizaki75 <153334358+yoshizaki75@users.noreply.github.com>
2024-02-18 23:21:48 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+ <html lang="ja">
+ 
+ <head>
+     <meta charset="UTF-8">
+     <title>PHP基礎編</title>
+ </head>
+ 
+ <body>
+     <p>
+        <?php
+        $vegetables = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+        print_r($vegetables);
+        ?>
+    </p>
+</body>
+
+</html>
