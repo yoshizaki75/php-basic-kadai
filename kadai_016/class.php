22a3dcfb5f98ac773500cb7b19526153ab932594
yoshizaki75 <153334358+yoshizaki75@users.noreply.github.com>
2024-02-23 23:30:27 +0900

    Add files via upload

---
+++ kadai_016/class.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>kadai_016</title>
+</head>
+<body>
+    <p>
+    <?php
+    class Food {
+        private $name;
+        private $price;
+        public function show_price() {
+            echo $this->price . '<br>';
+        }
+        public function __construct(string $name, int $price) {
+            $this->name = $name;
+            $this->price = $price;
+        }
+    }
+    $food = new Food('potato', 250);
+    print_r($food);
+    echo '<br>';
+
+    class Animal {
+        public $name;
+        public $height;
+        public $weight;
+        public function show_height() {
+            echo $this->height . '<br>';
+        }
+        public function __construct(string $name, int $height, string $weight) {
+            $this->name = $name;
+            $this->height = $height;
+            $this->weight = $weight;
+        }
+    }
+    $animal = new Animal('dog', 60, 5000);
+    print_r($animal);
+    echo '<br>';
+    $food->show_price();
+    $animal->show_height();
+    ?>
+    </p>
+</body>
+</html>
